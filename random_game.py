af6509b1fe35be28ac99ef17b5618aa90dda8995
alexsem88 <52209409+alexsem88@users.noreply.github.com>
2025-05-28 14:09:32 +0300

    Update random_game.py

--- random_game.py
+++ random_game.py
@@ -2,8 +2,10 @@ from random import randint
 from time import sleep
 import colorama
 from colorama import Fore, Style
+from tkinter import *
+from tkinter import ttk 
 
-delay = 5
+delay = 1
 
 class User:
     # конструктор
@@ -13,55 +15,95 @@ class User:
     health = 30
     armor =2
 
-user1 = User(name="Миша")
-user2 = User(name="Саша")
+user1 = User(name="Саша")
+user2 = User(name="Миша")
 
-user1.power = randint(5, 30)
-user2.power = randint(5, 30)
+user1.power = randint(15, 40)
+user2.power = randint(15, 40)
 user1.health = randint(40, 100)
 user2.health = randint(40, 100)
-user1.armor = randint(4, 20)
-user2.armor = randint(4, 20)
-
-print("Знакомьтесь с участниками турнира!")
-print("Первого бойца зовут", user1.name)
-print("Его характеристики:", "Сила =", user1.power, "Жизни =", user1.health, "Броня = ", user1.armor)
-print("Второго бойца зовут", user2.name)
-print("Его характеристики:", "Сила =", user2.power, "Жизни =", user2.health, "Броня = ", user2.armor)
-print("Начинаем битву!!! \n")
+user1.armor = randint(4, 8)
+user2.armor = randint(4, 8)
 
 def user1_hit():
     power_hit = randint(1, user1.power)
-    print(Fore.RED + user1.name, "ударил с силой =", power_hit, Style.RESET_ALL)
+    label5["text"] = f"{user1.name} ударил с силой {power_hit}"
     if power_hit <= user2.armor:
-        print(user2.name, "отразил удар благодатя своей броне")
+        label_u2_ar["text"] = f"{user2.name} отразил удар благодатя своей броне"
         power_hit = user2.armor
+    else:
+        label_u2_ar["text"] = f"{user2.name} смягчил удар благодаря своей броне"
     user2.health = user2.health - (power_hit - user2.armor)
+    label6["text"] = f"У бойца {user2.name} осталось жизней = {user2.health}"
 
 def user2_hit():
     power_hit = randint(1, user2.power)
-    print(Fore.BLUE + user2.name, "ударил с силой =", power_hit, Style.RESET_ALL)
+    label7["text"] = f"{user2.name} ударил с силой {power_hit}"
     if power_hit <= user1.armor:
-        print(user1.name, "отразил удар благодатя своей броне")
+        label_u1_ar["text"] = f"{user1.name} отразил удар благодаря своей броне"
         power_hit = user1.armor
+    else:
+        label_u1_ar["text"] = f"{user1.name} смягчил удар благодаря своей броне"
     user1.health = user1.health - (power_hit - user1.armor)
+    label8["text"] = f"У бойца {user1.name} осталось жизней {user2.health}"
 
-# Основной игровой цикл
-while user1.health >= 0 and user2.health >= 0:
-
+def users_hit():
     user1_hit()
     if user2.health <= 0:
-        break
-    print("У бойца", user2.name, "осталось жизней = ", user2.health, "\n")
-    sleep(delay)
-
+        user2.health = 0
+        label_win["text"] = f"Боец {user1.name} победил!"
+        btn["state"] = "disable"
+        label5.pack_forget()
+        label6.pack_forget()
+        label7.pack_forget()
+        label8.pack_forget()
+        label_u1_ar.pack_forget()
+        label_u2_ar.pack_forget()
+    else:
+        label6["text"] = f"У бойца {user2.name} осталось жизней = {user2.health}"
     user2_hit()
     if user1.health <= 0:
-        break
-    print("У бойца", user1.name, "осталось жизней = ", user1.health, "\n")
-    sleep(delay)
+        user2.health = 0
+        label_win["text"] = f"Боец {user2.name} победил!"
+        btn["state"] = "disable"
+        label5.pack_forget()
+        label6.pack_forget()
+        label7.pack_forget()
+        label8.pack_forget()
+        label_u1_ar.pack_forget()
+        label_u2_ar.pack_forget()
+    else:
+        label8["text"] = f"У бойца {user1.name} осталось жизней = {user1.health}"
 
-if user1.health <= 0:
-    print(user2.name, "победил!!!")
-elif user2.health <=0:
-    print(user1.name, "победил!!!")
+# Добавляем графику
+root = Tk()
+root.title("Game: Random Fight")
+root.geometry("400x400") 
+label1 = Label(text=f"Первого бойца зовут {user1.name}")
+label2 = Label(text=f"Его характерискики: Сила = {user1.power}, Жизни = {user1.health}, Броня = {user1.armor}")
+label3 = Label(text=f"Второго бойца зовут {user2.name}")
+label4 = Label(text=f"Его характерискики: Сила = {user2.power}, Жизни = {user2.health}, Броня = {user2.armor}")
+label1.pack()
+label2.pack()
+label3.pack()
+label4.pack()
+
+btn = ttk.Button(text="В бой", command=users_hit)
+btn.pack()
+
+label5 = Label()
+label6 = Label()
+label7 = Label()
+label8 = Label()
+label_u1_ar = Label()
+label_u2_ar = Label()
+label_win = Label()
+label5.pack(anchor=W) # user1 power_hit
+label_u2_ar.pack(anchor=E) # user2 ar
+label6.pack(anchor=E) # user2 health
+label7.pack(anchor=W) # user2 power_hit
+label_u1_ar.pack(anchor=E) # user1 ar
+label8.pack(anchor=E) # user1 health
+label_win.pack()
+
+root.mainloop()
